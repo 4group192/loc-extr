733ff8611b5156928a6c99bc6947431bfba53ab6
fanaev <81157248+fanaev@users.noreply.github.com>
2022-02-14 19:17:42 +0300

    fix

--- Classes/sympy_extremum.py
+++ Classes/sympy_extremum.py
@@ -14,7 +14,7 @@ class Extremum:
     Attributes
     ----------
     x, y: np.array
-        Массив X, Y. Пределы этих массивов устанавливает пользователь (limits), по умолчанию пределы: [-1000, 1000]
+        Массив X, Y. Пределы этих массивов устанавливает пользователь (limits), по умолчанию пределы: [-5, 5] 
     X, Y: np.array
         Массивы координатных сеток 2-мерного координатного пространства для массивов X и Y. (Преобразованные массивы для работы
         с 2-мерной функцией)
@@ -28,11 +28,11 @@ class Extremum:
     Methods
     -------
     extremums(self)
-        Находит все критические точки и определяет их типы
+        Безусловный экстремум. Находит все критические точки и определяет их типы (g(x,y) - отсутсвует).
     visualize(self)
         Строит график целевой функции и наносит на него найденные критические точки
     """
-    def __init__(self, variables: str, func, limits=None):
+    def __init__(self, variables: str, func, g = None, limits=None):
         """
         Parametres
         ----------
@@ -43,43 +43,44 @@ class Extremum:
         limits: list
             Ограничения для переменных
         """
-        if limits is None:
-            self.x = np.arange(-1000, 1000, 0.1)
-            self.y = np.arange(-1000, 1000, 0.1)
-        else:
-            if limits[0] is not None:
-                assert limits[0][0] < limits[0][1]
-                self.x = np.arange(limits[0][0], limits[0][1], 0.1)
+
+        assert limits[0][0] < limits[0][1]
+        self.x = np.arange(limits[0][0], limits[0][1], 0.1)
                 
-            if limits[1] is not None:
-                assert limits[1][0] < limits[1][1]
-                self.y = np.arange(limits[1][0], limits[1][1], 0.1)
+        assert limits[1][0] < limits[1][1]
+        self.y = np.arange(limits[1][0], limits[1][1], 0.1)
 
         self.X, self.Y = np.meshgrid(self.x, self.y)
         self.z = func(self.X, self.Y)
+
         self.symb_1, self.symb_2 = symbols(variables)
         self.analytic_func = func(self.symb_1, self.symb_2)
+        self.g = g(self.symb_1, self.symb_2) if g is not None else None
+
+        self.df_points = None #ans
 
     def extremums(self):
-        """Находит все критические точки и опредляет их типы
+        """g(x) - ограничивающая функция отсутсвует
+        Находит все критические точки и опредляет их типы
 
         Returns:
             pandas.Dataframe
         """
+        self.df_points = pd.DataFrame(columns=['x', 'y', 'z', 'type']) 
+
         diff_x = self.analytic_func.diff(self.symb_1)
         diff_y = self.analytic_func.diff(self.symb_2)
-        critical_values = solve([diff_x, diff_y], [self.symb_1, self.symb_2])
+        critical_values = solve([diff_x, diff_y], [self.symb_1, self.symb_2], dict = True)
 
         diff_xx = diff_x.diff(self.symb_1)
         diff_xy = diff_x.diff(self.symb_2)
         diff_yy = diff_y.diff(self.symb_2)
 
-        self.df_points = pd.DataFrame(columns=['x', 'y', 'val', 'type'])
         func_silv = diff_xx * diff_yy - diff_xy**2
         for point in critical_values:
-            A = diff_xx.subs(self.symb_1, point[0]).subs(self.symb_2, point[1])
-            val = func_silv.subs(self.symb_1, point[0]).subs(self.symb_2, point[1])
-            row = {'x': float(point[0]), 'y': float(point[1]), 'val': float(val)}
+            A = diff_xx.subs(self.symb_1, point[self.symb_1]).subs(self.symb_2, point[self.symb_2])
+            val = func_silv.subs(self.symb_1, point[self.symb_1]).subs(self.symb_2, point[self.symb_2])
+            row = {'x': float(point[self.symb_1]), 'y': float(point[self.symb_2]), 'z': float(self.analytic_func.subs(self.symb_1, point[self.symb_1]).subs(self.symb_2, point[self.symb_2]))}
             if val == 0:
                 row['type'] = 'Требуется доп. исследование'
             elif val < 0:
@@ -92,12 +93,43 @@ class Extremum:
             self.df_points = self.df_points.append(row, ignore_index = True)
         return self.df_points
 
+    def lagr(self):
+        """Задача условного экстремума
+        Находит все критические точки и опредляет их типы
+
+        Returns:
+            pandas.DataFrame
+        """
+        w = Symbol('w')
+        L = self.analytic_func + w*self.g
+        L_x, L_y= L.diff(self.symb_1), L.diff(self.symb_2)
+        g_x, g_y = self.g.diff(self.symb_1), self.g.diff(self.symb_2)
+        L_xx, L_yy, L_xy = L_x.diff(self.symb_1), L_y.diff(self.symb_2), L_y.diff(self.symb_1)
+        critical_values = solve([L_x, L_y, self.g], [self.symb_1, self.symb_2, w], dict = True)
+        self.df_points = pd.DataFrame(columns=['x', 'y', 'z', 'type'])
+        f_subs = lambda f: f.subs([(self.symb_1, point[self.symb_1]), (self.symb_2, point[self.symb_2]), (w, point[w])])
+        for point in critical_values:
+            row = {'x': float(point[self.symb_1]), 'y': float(point[self.symb_2]), 'z': float(f_subs(self.analytic_func))}
+            A = np.linalg.det(np.array([
+                [0, f_subs(g_x), f_subs(g_y)],
+                [f_subs(g_x), f_subs(L_xx), f_subs(L_xy)],
+                [f_subs(g_y), f_subs(L_xy), f_subs(L_yy)]
+            ], dtype=float))
+            if A > 0:
+                row['type'] = 'Max'
+            elif A < 0:
+                row['type'] = 'Min'
+            else:
+                row['type'] = 'Седловая точка'
+            self.df_points = self.df_points.append(row, ignore_index = True)
+        return self.df_points
+        
     def visualize(self):
         """Строит график целевой функции и наносит на него найденные критические точки
         """
         fig = go.Figure(data=[go.Surface(z=self.z, x=self.x, y=self.y)])
         for  point_type in set(self.df_points.type):
-            z = self.df_points[self.df_points.type == point_type].val
+            z = self.df_points[self.df_points.type == point_type].z
             x = self.df_points[self.df_points.type == point_type].x
             y = self.df_points[self.df_points.type == point_type].y
             fig.add_trace(go.Scatter3d(z = z, x = x, y= y, name = point_type, showlegend = True))
@@ -110,6 +142,6 @@ class Extremum:
 
 
 if __name__ == '__main__': 
-    Example1 = Extremum('x y', lambda x, y: y*(x**2)+x*(y**3) - x*y, limits=[[-10, 10], [-10, 10]])
-    print(Example1.extremums())
+    Example1 = Extremum('x y', lambda x, y: x*y, lambda x,y: x**2+4*y**2 - 1 ,limits=[[-10, 10], [-10, 10]])
+    print(Example1.lagr())
     Example1.visualize().show()
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import streamlit as st
-from Classes import *
 from Classes.sympy_extremum import Extremum
 
-#Настройка условий
-Example1 = Extremum('x y', lambda x, y: y*(x**2)+x*(y**3) - x*y, limits=[[-10, 10], [-10, 10]])
-
-
 st.title('Поиск экстремумов ФНП')
-st.header('Первое задание, пункт 1')
-st.write('Найденные экстремумы:')
-st.dataframe(Example1.extremums())
-st.plotly_chart(Example1.visualize())
+st.header('Первое задание. Пункт 1.')
+#Настройка условий
+variables = st.text_input('Введите название переменных, например x y', value = 'x y')
+func = st.text_input('Введите целевую функцию', value = 'y*(x**2)+x*(y**3) - x*y')
+task = st.selectbox(label = 'Выберите тип задачи', options = ['Безусловный экстремум', 'Условный экстремум'])
+if task == 'Условный экстремум':
+    g = st.text_input('Введите ограничивающую функцию', value = 'x**2+4*y**2 - 1')
+x_d = st.number_input('Введите нижнюю границу для x', value = -10)
+x_up = st.number_input('Введите верхнюю границу для x', value = 10)
+y_d = st.number_input('Введите нижнюю границу для y ', value = -1)
+y_up = st.number_input('Введите верхнюю границу для y ', value= 1)
+limits = [[x_d, x_up], [y_d, y_up]]
+
+if st.button('Найти экстремумы и построить график'):
+    if task =='Условный экстремум':
+        Example = Extremum(variables = variables, func = lambda x, y: eval(func), g = lambda x, y: eval(g), limits = limits)
+        st.dataframe(Example.lagr())
+        st.plotly_chart(Example.visualize())
+    else:
+        Example = Extremum(variables = variables, func = lambda x, y: eval(func), limits = limits)
+        st.dataframe(Example.extremums())
+        st.plotly_chart(Example.visualize())
+
