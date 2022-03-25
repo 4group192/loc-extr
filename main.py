8f4a3f131001c3097b0f85e71f241fcd16843f77
fanaev <81157248+fanaev@users.noreply.github.com>
2022-03-26 01:35:38 +0300

    ТЗ № 3, градиентные методы

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
-from pages import *
-project = st.sidebar.selectbox("Выберите проект", options = ['Поиск экстремумов ФНП', 'Методы одномерной оптимизации'])
+from Classes.streamlit_code import *
+project = st.sidebar.selectbox("Выберите проект", options = [
+    'Поиск экстремумов ФНП', 
+    'Методы одномерной оптимизации',
+    'Градиентные методы многомерной оптимизации'])
+    
 projects = {'Поиск экстремумов ФНП': page1,
-            'Методы одномерной оптимизации': page2
+            'Методы одномерной оптимизации': page2,
+            'Градиентные методы многомерной оптимизации': page3
 }
 projects[project]()
